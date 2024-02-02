13a2bc39c0e5ec30f9d3417a3c98c39e9332619f
BASAVARAJ IAS <Basavarajbadiger>
2024-02-02 16:12:59 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Hero from './components/Hero/Hero.jsx'
+import Navbar from './components/Navbar/TheNavbar.jsx';
+function App() {
+  return (
+  <>
+<Navbar/>
+<Hero/>
+
+  </>
+  );
+}
+
+export default App;
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Search.module.css";
+import { ReactComponent as SearchIcon } from "../assests/searchicon.svg";
+
+export default function Search({ placeholder }) {
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+      <form className={styles.wrapper} onSubmit={onSubmit}>
+        <input className={styles.search} placeholder={placeholder} required />
+        <button type="submit" className={styles.searchButton}>
+          <SearchIcon/>
+        </button>
+      </form>
+  );
+}
